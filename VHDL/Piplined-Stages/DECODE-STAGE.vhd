d4dbb0530c4735c15222160bd9eaee902e72f936
AymanAzzam <aymanazzam63@gmail.com>
2020-05-07 04:58:50 +0200

    Rename decode entity

--- VHDL/Piplined-Stages/DECODE-STAGE.vhd
+++ VHDL/Piplined-Stages/DECODE-STAGE.vhd
@@ -25,7 +25,7 @@ use ieee.std_logic_1164.all;
 --	jump_cat,	(1 bit),	uncond_jump	(1 bit)
 --	ir_hazard	(),		src_hazard	(2 bits)
 
-entity DECODER is
+entity decode_stage is
 port(	pc_in,ir,dst1_result,dst2_result:		in std_logic_vector(31 downto 0);
 	dst1_num,dst2_num:				in std_logic_vector(2 downto 0);
 	src1_exist,src2_exist,dst1_en,dst2_en:		in std_logic;
@@ -38,4 +38,4 @@ port(	pc_in,ir,dst1_result,dst2_result:		in std_logic_vector(31 downto 0);
 	wb,m:						out std_logic_vector(3 downto 0);
 	ex:						out std_logic_vector(4 downto 0)
 );
-end DECODER;
+end decode_stage;
