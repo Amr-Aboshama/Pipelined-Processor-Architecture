0fd5760317e8195aece5db77a410c192cfd49d66
AymanAzzam <aymanazzam63@gmail.com>
2020-05-07 07:41:41 +0200

    done EXT inside Decode

--- VHDL/Piplined-Stages/DECODE-STAGE.vhd
+++ VHDL/Piplined-Stages/DECODE-STAGE.vhd
@@ -14,7 +14,7 @@ use ieee.std_logic_1164.all;
 --	interrupt	(1 bit),	hazard_detected	(1 bit)
 
 -- Output bits to ID/IE
---	Extend		(32 bits)	
+--	ext		(32 bits)	
 --	Rsrc2 		(32 bits),	Rsrc1 		(32 bits)
 --	Rscr2_num 	(3 bits),	Rsrc1_num 	(3 bits)
 --	Rdst1_num 	(3 bits),	Rdst2_num 	(3 bits)
@@ -30,7 +30,7 @@ port(	ir,dst1_result,dst2_result:			in std_logic_vector(31 downto 0);
 	enable,src1_exist,src2_exist,dst1_en,dst2_en:	in std_logic;
 	clk, rst,intr,zero_flag,hazard_detected:	in std_logic;
 
-	extend,Rsrc2,Rsrc1:				out std_logic_vector(31 downto 0);
+	ext,Rsrc2,Rsrc1:				out std_logic_vector(31 downto 0);
 	jump_cat,uncond_jump:			out std_logic;
 	src_hazard:					out std_logic_vector(1 downto 0);
 	Rsrc1_num,Rsrc2_num,Rdst1_num,Rdst2_num:	out std_logic_vector(2 downto 0);
@@ -64,9 +64,9 @@ begin
 
 	ex(4 downto 0) <= "10000" when ir(31 downto 27) = "11000"			else	--JZ
 			  ir(31 downto 27) when 
-			  (ir(31 downto 30) = "01" and ir(29 downto 27) /= "100") or 	 	--AND, OR, ADD, SUB, IADD, SHL or SHR
+			  (ir(31 downto 30) = "01" and ir(29 downto 27) /= "100") or 	 	--AND - OR - ADD - SUB - IADD - SHL - SHR
 			  (ir(31 downto 30) = "00" and ((ir(29) xor ir(28))='1') and 
-			  				((ir(29) xor ir(28))='1')) 	else 	--NOT, INC or DEC
+			  				((ir(29) xor ir(28))='1')) 	else 	--NOT - INC - DEC
 			  "00000";
 
  	m <= 	"0101" when 	ir(31 downto 27) = "10000" or 		--PUSH
@@ -80,17 +80,22 @@ begin
 		"0000";
 	
 	wb <=	"00111" when	ir(31 downto 27) = "01100"	else	--SWAP
-		"00101" when	( ir(31 downto 30) = "00" and 
-				  ir(28 downto 27) /= "00" ) or		--NOT or INC or DEC or IN 
-				( ir(31 downto 30) = "01" and 
-				  ir(29 downto 27) /= "100" )	else	--AND or OR or ADD or SUB or IADD or SHL or SHR
+		"00101" when	(ir(31 downto 30) = "00" and 
+				  ir(28 downto 27) /= "00") or		--NOT - INC - DEC - IN 
+				(ir(31 downto 30) = "01" and 
+				  ir(29 downto 27) /= "100" )	else	--AND - OR - ADD - SUB - IADD - SHL - SHR
 		"00100" when	ir(31 downto 27) = "10001" or		--POP
 				ir(31 downto 27) = "10110"	else	--LDD
 		"00001" when	ir(31 downto 27) = "10111"	else	--LDM
 		"01000" when	ir(31 downto 27) = "11000"	else	--JZ
 		"11000" when	ir(31 downto 30) = "11" and 
-				ir(29 downto 27) /= "000"	else	--JMP or CALL or RET or RTI
+				ir(29 downto 27) /= "000"	else	--JMP - CALL - RET - RTI
 		"00000";
 		
-
+	ext <=	"0000000000000000" & ir(15 downto 0) when 
+			(ir(31 downto 29) = "011" and ((ir(28) or ir(27))='1')) or	--IADD - SHL - SHR
+			ir(31 downto 27) = "10111"	else	--LDM
+		"000000000000" & ir(19 downto 0) when 
+			ir(31 downto 27) = "10101" or ir(31 downto 27) = "10110" else	--STD - LDD
+		(others => '0');
 end decode;
